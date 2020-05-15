e32bcdc838ec456816e0fed9a4d80fa152dedc73
tamim0508 <64509358+tamim0508@users.noreply.github.com>
2020-05-14 18:55:14 -0700

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+class ground {
+constructor(x,y,width,height){
+var options ={
+isStatic: true
+
+}
+this.width = width;
+this.height = height;
+this.body = Bodies.rectangle(x,y,width,height,options)
+World.add(world,this.body) 
+
+}
+display(){
+var pos =this.body.position;
+fill("blue");
+rectMode(CENTER);
+rect(pos.x,pos.y,this.width,this.height)
+
+
+}
+
+
+
+}
